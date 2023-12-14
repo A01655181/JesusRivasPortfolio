69091220789d644b795bc76cc1382ffed2404f28
ShushoRiv <73494815+A01655181@users.noreply.github.com>
2023-12-14 15:56:28 -0600

    Dec 14 2023 - Player Animations
    Player animatons were added and a first version of the animation controller. Needs some tweeks but it will work for now.

--- JesusRivasPortfolio/Assets/Scripts/PlayerController.cs
+++ JesusRivasPortfolio/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO MAKE A DEDICATED SCRIPT FOR ANIMATION
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
@@ -9,10 +11,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce; 
     private bool isJumping;
     private bool canJump;
+
+    // ANIMATION VARIABLES
+    [SerializeField] private Animator animatorController;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animatorController = GetComponent<Animator>();
         isJumping = false;
         canJump = true;
     }
@@ -39,7 +46,34 @@ public class PlayerController : MonoBehaviour
         else if(isJumping == false)
         {
             rb.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+            
+            if(rb.velocity.x > 0)
+            {
+                animatorController.SetBool("isMoving", true);
+                FlipRight();
+            }
+            else if(rb.velocity.x < 0)
+            {
+                animatorController.SetBool("isMoving", true);
+                FlipLeft();
+            }
+            else
+            {
+                animatorController.SetBool("isMoving", false);
+            }
         }
+    }
+
+    private void FlipLeft()
+    {
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        rend.flipX = true;
+    }
+
+    private void FlipRight()
+    {
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        rend.flipX = false;
     }
 
 
